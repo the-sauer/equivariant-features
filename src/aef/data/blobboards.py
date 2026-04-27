ddd7a523d8cf0dd4f866ae5076842f2db30351cf
Hendrik Sauer <hendrik-sauer@mailbox.org>
2026-04-27 16:29:30 +0200

    fix: update gitignore and add data module

---
+++ src/aef/data/blobboards.py
@@ -0,0 +1,30 @@
+# Affine Equivariant Features, the main implementation of my master thesis.
+# Copyright (C) 2026 Hendrik Sauer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import blobboards
+import torch
+
+from ..data import HomographyData
+
+
+class BlobBoardData(HomographyData):
+    def __init__(self, num_boards, image_size=(128, 128), **kwargs):
+        kwargs["in_memory"] = True
+        super().__init__(
+            torch.stack([torch.Tensor(blobboards.blob_pattern(*image_size).pattern).unsqueeze(0) for _ in range(num_boards)]),
+            image_size=image_size,
+            **kwargs
+        )
